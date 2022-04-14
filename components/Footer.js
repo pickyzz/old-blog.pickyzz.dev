d2c58f0ec47413a8c8b05c466025a7aac30a418f
pickyzz <pikcolokung@gmail.com>
2022-04-15 03:32:05 +0700

    status

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,4 @@
-import { faCopyright, faEye, faUsers } from '@fortawesome/free-solid-svg-icons'
+import { faCopyright, faEye, faUsers, faServer } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 // import Link from 'next/link'
@@ -12,12 +12,12 @@ const Footer = ({ title }) => {
     <footer
       className='bg-rose-800 dark:bg-black dark:border-gray-900 border-t flex-shrink-0 justify-center text-center m-auto w-full text-gray-400 text-sm p-6'
     >
-      <FontAwesomeIcon icon={faCopyright} /> <span> {`${from} - ${y}`} | <a href={BLOG.link} className='font-bold text-gray-100'>{BLOG.author}</a> | Powered by <a target='_blank' rel='noreferrer' href='https://notion.so' className='font-bold text-gray-100'>❤</a></span>
+      <FontAwesomeIcon icon={faCopyright} /> <span> {`${from} - ${y}`} | <a href={BLOG.link} className='font-regular text-gray-100'>{BLOG.author}</a> | <a target='_blank' rel='noreferrer' href='https://status.pickyzz.dev/' className='pl-auto font-regular text-gray-100'><FontAwesomeIcon icon={faServer}/> status</a></span>
       <span className='hidden busuanzi_container_site_pv'>
             <FontAwesomeIcon icon={faEye}/><span className='px-1 busuanzi_value_site_pv'> </span>pv
       </span>
       <span className='pl-2 hidden busuanzi_container_site_uv'>
-        <FontAwesomeIcon icon={faUsers}/> <span className='px-1 busuanzi_value_site_uv'> </span>uv   </span>
+        <FontAwesomeIcon icon={faUsers}/> <span className='px-1 busuanzi_value_site_uv'> </span>uv</span>
         <br/>
         {/* <h1>{title}</h1> */}
     </footer>
