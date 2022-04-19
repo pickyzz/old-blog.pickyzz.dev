245defab42132a62ccf121284aa5e9b0551cd932
pickyzz <pikcolokung@gmail.com>
2022-04-19 16:15:46 +0700

    fix image hover style

--- components/BlogPostCard.js
+++ components/BlogPostCard.js
@@ -41,8 +41,8 @@ const BlogPostCard = ({ post }) => {
 
       {post?.page_cover && (
         <Link href={`${BLOG.path}/article/${post.slug}`} passHref>
-        <div className='h-60 w-full xl:max-w-xs relative xl:h-full duration-200 cursor-pointer transform overflow-hidden'>
-          <Image className='animate__animated animate__zoomIn hover:scale-105 transform duration-700' src={post?.page_cover} alt={post.title} layout='fill' objectFit='cover' loading='lazy' />
+        <div className='animate__animated animate__zoomIn h-60 w-full xl:max-w-xs relative xl:h-full duration-200 cursor-pointer transform overflow-hidden'>
+          <Image className='hover:scale-105 transform duration-700' src={post?.page_cover} alt={post.title} layout='fill' objectFit='cover' loading='lazy' />
         </div>
       </Link>
       )}
