6a12fd82e21ce4b3d9b477d64fd3b58f683e6e11
pickyzz <pikcolokung@gmail.com>
2022-05-18 02:19:58 +0700

    discord status on profileCard

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Router from 'next/router'
 import React from 'react'
 import SocialButton from './SocialButton'
+import DiscordData from '@/lib/discordData'
 
 const InfoCard = ({ postCount }) => {
   return (
@@ -10,21 +11,26 @@ const InfoCard = ({ postCount }) => {
       <div className="flex flex-col items-center justify-center">
         <div
           id="profileImage"
-          className="hover:scale-105 transform duration-200 cursor-pointer"
+          className="hover:scale-[103%] transform duration-200 cursor-pointer"
           onClick={() => {
             Router.push('/about')
           }}
         >
           <Image
             alt={BLOG.title}
-            width={112}
-            height={112}
+            width={120}
+            height={120}
             loading="lazy"
             src={BLOG.avatar}
             className="rounded-full"
           />
+          <div className="-mt-[20%] text-center">
+            <div className="absolute top-29 right-4">
+              <DiscordData />
+            </div>
+          </div>
         </div>
-        <div className="text-2xl font-mono dark:text-white py-2 -mb-4">
+        <div className="mt-6 text-2xl font-mono dark:text-white py-2 -mb-4">
           {BLOG.author}
         </div>
         <div className="font-light dark:text-white py-2 mb-2">{BLOG.bio}</div>
