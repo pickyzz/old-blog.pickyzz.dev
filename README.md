6f630c4a1dff5bd65f735b15b9bbe05d1eb7f1ad
pickyzz <pikcolokung@gmail.com>
2022-05-18 17:29:12 +0700

    add Spotify status icon

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ### resource of pickyzz.dev
 
 - Build with NextJs and use Notion as content service management.
-- Use Discord avatar & online status from Lynard discord rest api.
+- Use Discord avatar & online status from Lanyard discord rest api.
 
 Original theme by [tangly1024.com](https://tangly1024.com)
--- components/MenuButtonGroup.js
+++ components/MenuButtonGroup.js
@@ -3,7 +3,12 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useGlobal } from '@/lib/global'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArchive, faHome, faInfoCircle, faTag } from '@fortawesome/free-solid-svg-icons'
+import {
+  faArchive,
+  faHome,
+  faInfoCircle,
+  faTag
+} from '@fortawesome/free-solid-svg-icons'
 import BLOG from 'blog.config'
 
 const MenuButtonGroup = ({ allowCollapse = false }) => {
@@ -11,9 +16,27 @@ const MenuButtonGroup = ({ allowCollapse = false }) => {
   const router = useRouter()
   const links = [
     { id: 0, icon: faHome, name: locale.NAV.INDEX, to: '/' || '/', show: true },
-    { id: 1, icon: faInfoCircle, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
-    { id: 3, icon: faTag, name: locale.NAV.TAG, to: '/tag', show: BLOG.showArchive },
-    { id: 3, icon: faArchive, name: locale.NAV.ARCHIVE, to: '/archive', show: BLOG.showArchive }
+    {
+      id: 1,
+      icon: faInfoCircle,
+      name: locale.NAV.ABOUT,
+      to: '/about',
+      show: BLOG.showAbout
+    },
+    {
+      id: 3,
+      icon: faTag,
+      name: locale.NAV.TAG,
+      to: '/tag',
+      show: BLOG.showArchive
+    },
+    {
+      id: 3,
+      icon: faArchive,
+      name: locale.NAV.ARCHIVE,
+      to: '/archive',
+      show: BLOG.showArchive
+    }
     // { id: 4, icon: faRss, name: locale.NAV.RSS, to: '/feed', show: true }
     // { id: 7, icon: 'faGithub', name: 'Github', to: 'https://github.com/tangly1024', show: true },
     // { id: 5, icon: 'faWeibo', name: '微博', to: 'https://weibo.com/tangly1024', show: true },
@@ -23,25 +46,36 @@ const MenuButtonGroup = ({ allowCollapse = false }) => {
     // { id: 8, icon: 'faTwitter', name: 'Twitter', to: 'https://twitter.com/troy1024_1', show: true },
     // { id: 9, icon: 'faTelegram', name: 'Telegram', to: 'https://t.me/tangly_1024', show: true }
   ]
-  return <nav id='nav'>
-    <div className='leading-8 text-gray-500 dark:text-gray-400'>
-      {links.map(link => {
-        if (link.show) {
-          const selected = (router.pathname === link.to) || (router.asPath === link.to)
-          return <Link key={link.id + link.icon} title={link.to} href={link.to} >
-            <a className={'py-1 my-1 px-5 mx-2 duration-300 text-base hover:bg-gray-500 hover:text-white hover:shadow-lg cursor-pointer font-light flex flex-nowrap items-center ' +
-              (selected ? 'bg-gray-200 text-black dark:bg-gray-600 dark:text-white' : ' ')} >
-              <div className='my-auto justify-center flex '>
-                <FontAwesomeIcon icon={link.icon} />
-              </div>
-              <div className={'ml-4'}>{link.name}</div>
-            </a>
-          </Link>
-        } else {
-          return <></>
-        }
-      })}
-    </div>
-  </nav>
+  return (
+    <nav id="nav">
+      <div className="leading-8 text-gray-500 dark:text-gray-400">
+        {links.map(link => {
+          if (link.show) {
+            const selected =
+              router.pathname === link.to || router.asPath === link.to
+            return (
+              <Link key={link.id + link.icon} title={link.to} href={link.to}>
+                <a
+                  className={
+                    'py-1 my-1 px-5 mx-2 duration-300 text-base hover:bg-gray-500 hover:text-white hover:shadow-lg cursor-pointer font-light flex flex-nowrap items-center ' +
+                    (selected
+                      ? 'bg-gray-200 text-black dark:bg-gray-600 dark:text-white'
+                      : ' ')
+                  }
+                >
+                  <div className="my-auto justify-center flex ">
+                    <FontAwesomeIcon icon={link.icon} />
+                  </div>
+                  <div className={'ml-4'}>{link.name}</div>
+                </a>
+              </Link>
+            )
+          } else {
+            return null
+          }
+        })}
+      </div>
+    </nav>
+  )
 }
 export default MenuButtonGroup
