6c82262fae57be076516f6ded1197c47dc8fc586
pickyzz <pikcolokung@gmail.com>
2022-06-04 01:52:27 +0700

    consent translate

--- components/cookieConsent.js
+++ components/cookieConsent.js
@@ -10,25 +10,27 @@ export default function CookieAcception() {
     <CookieConsent
       location="bottom"
       flipButtons
-      buttonText="I understand"
+      buttonText="ยินยอม (Agree)"
       cookieName="cookieConsent"
       style={{ background: '#2B373B' }}
       buttonStyle={{ color: '#4e503b', fontSize: '16px' }}
       enableDeclineButton
-      declineButtonText="Decline"
+      declineButtonText="ไม่ยินยอม (Decline)"
       onDecline={() => {
         cookie.remove('theme')
         cookie.remove('cookieConsent')
       }}
     >
-      We use cookies to enhance your experience.{' '}
+      เว็บไซต์นี้มีการใช้ Cookies
+      ในการเก็บการตั้งค่าธีมและเพิ่มประสิทธิภาพในการใช้งานเว็บไซต์
+      ทั้งนี้ขึ้นอยู่กับความยินยอมของผู้ใช้งานตามหลัก PDPA{' '}
       <span style={{ fontSize: '10px' }}>
         <a
           target="_blank"
           rel="noreferrer"
-          href="https://www.onetrust.com/blog/the-ultimate-guide-to-thai-pdpa-compliance/"
+          href="https://t-reg.co/blog/t-reg-knowledge/what-is-pdpa/"
         >
-          More info
+          เพิ่มเติม
         </a>
       </span>
     </CookieConsent>
